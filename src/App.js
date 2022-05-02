65c1590f43e70ed9a85c0a07d5ca1dac3acd941b
WilliamDenega <williamdenega@gmail.com>
2022-05-02 09:45:46 -0400

    commit tree 8a26d3692d2724ba8f7f4a19cf028ddd4f92862f

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import React, { useState, useEffect } from "react";
-
+import DogPics from "./DogPics";
 function App() {
-  useEffect(() => {
-    console.log("useEffect called");
-  });
+
+  const [count, setCount ] = useState(0)
+  const [text, setText ] = useState('')
+
+  useEffect(()=>{
+    document.title = text
+  }, [text])
+
+  useEffect(
+    () => {
+    setTimeout(()=> setCount(0), 5000)
+  }
+,[]);
 
   console.log("Component rendering");
 
-  return <button>Click Me</button>;
+  return <div>
+    <button 
+      onClick={()=>setCount((count) => count+1)}>I've been clikced {count} times
+    </button>;
+    <input 
+      type="text" 
+      placeholder="Type away..."   
+      value={text}
+      onChange={(e)=>setText((e.target.value))}
+    />
+    <DogPics />
+    </div>
+
 }
 
 export default App;
